72885af8f81df5dde71aa8a1c18ca1d4d48b5896
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-11-20 11:17:04 +0200

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/picture-gallery/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
